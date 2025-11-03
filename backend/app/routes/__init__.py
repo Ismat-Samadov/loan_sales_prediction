8cd14f2bd82426e006c062563102a030a2ba3d72
ismat-samadov <ismetsemedov@gmail.com>
2025-11-03 17:08:05 +0400

    init

--- backend/app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-# Routes package
-from . import analytics, statistics, predictions, insights
-
-__all__ = ['analytics', 'statistics', 'predictions', 'insights']
--- backend/app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Utils package
-from .data_loader import data_loader
-
-__all__ = ['data_loader']
